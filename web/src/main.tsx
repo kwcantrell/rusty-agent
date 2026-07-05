e7b4b103f3ba01f76646ffa0757e5f53f3893069
Kalen Cantrell <cantrell.kalen@gmail.com>
2026-07-05 11:03:36 -0700

    feat(web): add cli terminal design tokens and JetBrains Mono

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,5 +1,6 @@
 import "@fontsource-variable/inter";
 import "@fontsource-variable/fraunces";
+import "@fontsource-variable/jetbrains-mono";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
